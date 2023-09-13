8049a774091317927dd79b04aaac91cdd75d926e
Adrien Laporte <adri2@Adri2-Tour>
2023-09-13 13:32:11 +0200

    Projet Initial

---
+++ generator.py
@@ -0,0 +1,35 @@
+import os
+import csv
+import json
+from pathlib import Path
+
+res_path = Path(os.path.dirname(__file__)) / 'res'
+
+departements = {}
+
+# Prefectures.csv est un copié-collé du tableau de la page
+# https://fr.wikipedia.org/wiki/Liste_des_pr%C3%A9fectures_de_France
+with open(res_path/"prefectures.csv", encoding="utf8") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter='\t')
+    for row in reader:
+        info = {
+            "nom":row['Département'].strip(),
+            "prefecture":row['Préfecture'].strip(),
+        }
+        departements[row['NoInsee'].strip()] = info
+
+# Les fichiers JSON des régions ont été écrits à la main.
+with open(res_path/"regions.json", encoding="utf8") as file:
+    regions = json.load(file)
+    for reg, deps in regions.items():
+        for dep in deps:
+            departements[str(dep)]['region'] = reg
+
+with open(res_path/"regions2016.json", encoding="utf8") as file:
+    regions = json.load(file)
+    for reg, deps in regions.items():
+        for dep in deps:
+            departements[str(dep)]['region2016'] = reg
+
+with open(res_path/"departements.json", encoding="utf8", mode='w') as file:
+    json.dump(departements, file, indent=2, ensure_ascii=False)
---
+++ quiz.py
@@ -0,0 +1,48 @@
+#!/data/data/com.termux/files/usr/bin/python
+
+import os
+import json
+import subprocess
+from random import choice, getrandbits
+from pathlib import Path
+
+res_path = Path(os.path.dirname(__file__)) / 'res'
+
+# 1. Lecture des données et choix d'un département au hasard
+with open(res_path / 'departements.json', encoding='utf8') as file:
+    departements = json.load(file)
+
+numero = choice(list(departements.keys()))
+dep = departements[numero]
+
+# 2. Formatage du numéro si un seul chiffre, et choix d'une question à poser au hasard (nom ou numéro)
+if len(numero) == 1:
+    numero = '0'+numero
+
+nom = dep['nom']
+if getrandbits(1):
+    question = f"Numéro {numero} ?"
+else:
+    question = f"{nom} ?"
+
+# 3. Création de la réponse sous forme de texte
+reponse = f"""\
+{nom} ({numero}) :
+- Préfecture : {dep['prefecture']}
+"""
+if 'region' in dep:
+    reponse += f"- Ancienne région : {dep['region']}\n"
+if 'region2016' in dep:
+    reponse += f"- Nouvelle région : {dep['region2016']}"
+
+# 4. Envoi de la notification
+action = f"termux-dialog confirm -i '{reponse}'"
+subprocess.run([
+    "termux-notification",
+    "-t", "Quiz Département !!",
+    "-c", f"{question}",
+    "--action", action,
+    "--on-delete", action,
+])
+
+exit()
